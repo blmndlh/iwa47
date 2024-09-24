8e862211fba8e6c865ee9c839f663bfaa55b6216
blmndlh <57013837+blmndlh@users.noreply.github.com>
2024-09-24 20:25:00 +0200

    Update iwa47.js

--- iwa47.js
+++ iwa47.js
@@ -1,4 +1,4 @@
-d3.csv('https://github.com/blmndlh/iwa47/blob/main/iwa47.csv', function(err, rows){
+d3.csv('https://github.com/blmndlh/iwa47/main/iwa47.csv', function(err, rows){
   function unpack(rows, key) {
   return rows.map(function(row) { return row[key]});
 }
